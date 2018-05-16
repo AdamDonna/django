7f851c4bc8ba73ab8844759614649c14c8a770d3
Adam Donaghy <adamdonaghy1994@gmail.com>
2018-05-16 12:58:23 +1000

    Test to simulate invalid pk submisison

--- tests/admin_changelist/tests.py
+++ tests/admin_changelist/tests.py
@@ -13,7 +13,7 @@ from django.db.models.fields import Field, IntegerField
 from django.db.models.functions import Upper
 from django.db.models.lookups import Contains, Exact
 from django.template import Context, Template, TemplateSyntaxError
-from django.test import TestCase, override_settings
+from django.test import TestCase, override_settings, skipUnlessDBFeature
 from django.test.client import RequestFactory
 from django.urls import reverse
 from django.utils import formats
@@ -761,6 +761,26 @@ class ChangeListTests(TestCase):
         request = self.factory.post(changelist_url, data=data)
         pks = m.get_edited_object_pks(request, prefix='form')
         self.assertEqual(sorted(pks), sorted([str(a.pk), str(b.pk), str(c.pk)]))
+
+    @skipUnlessDBFeature('connection.features.has_native_uuid_field')
+    def test_type_error_on_invalid_pk_submission(self):
+        Swallow.objects.create(origin='Swallow A', load=4, speed=1)
+        data = {
+            'form-TOTAL_FORMS': '3',
+            'form-INITIAL_FORMS': '3',
+            'form-MIN_NUM_FORMS': '0',
+            'form-MAX_NUM_FORMS': '1000',
+            'form-0-uuid': 'INVALD_PRIMARY_KEY',
+        }
+
+        superuser = self._create_superuser('superuser')
+        self.client.force_login(superuser)
+        changelist_url = reverse('admin:admin_changelist_swallow_changelist')
+
+        self.assertRaises(
+            TypeError,
+            self.client.post(changelist_url, data, follow=True, extra={'o': '-2'})
+        )
 
     def test_deterministic_order_for_unordered_model(self):
         """
